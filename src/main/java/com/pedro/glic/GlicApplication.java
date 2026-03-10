d54d8ffd22cb2326eb478d8ab5e9c9b77f81169c
pedroccarv <pedrocarvalho.phm@gmail.com>
2026-03-10 11:19:04 -0300

    Initial commit

---
+++ src/main/java/com/pedro/glic/GlicApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.glic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pedro/glic/GlicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.glic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
