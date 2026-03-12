74510bd854084607e10362dc62c27741bd5cb30c
pedroccarv <pedrocarvalho.phm@gmail.com>
2026-03-12 14:12:45 -0300

    build: update project dependencies and spring boot configuration

--- src/main/java/com/pedro/glic/GlicApplication.java
+++ src/main/java/com/pedro/glic/GlicApplication.java
@@ -1,5 +1,6 @@
 package com.pedro.glic;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GlicApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
+
         SpringApplication.run(GlicApplication.class, args);
     }
 
