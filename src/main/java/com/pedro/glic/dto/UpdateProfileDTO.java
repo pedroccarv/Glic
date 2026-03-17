39d08d5914443e6c7915082a37616057295fe29f
pedroccarv <pedrocarvalho.phm@gmail.com>
2026-03-17 14:17:05 -0300

    feat: add UpdateProfileDTO for partial profile updates

--- src/main/java/com/pedro/glic/dto/UpdateProfileDTO.java
+++ src/main/java/com/pedro/glic/dto/UpdateProfileDTO.java
@@ -1,4 +1,17 @@
 package com.pedro.glic.dto;
 
-public class UpdateProfileDTO {
+import com.pedro.glic.enums.DiabetesType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.PositiveOrZero;
+
+import java.time.LocalDate;
+
+public record UpdateProfileDTO (
+        @NotNull @PastOrPresent LocalDate birthday,
+        @NotNull @PastOrPresent LocalDate diagnostic,
+        @NotNull String phone,
+        @PositiveOrZero Integer carbCounting,
+        @NotNull DiabetesType diabetesType
+) {
 }
